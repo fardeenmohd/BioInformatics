9824ff38f5453d892aec5d957a8b7dee2bd31cb8
fardeenmohd <fardeenmohdpl@gmail.com>
2018-10-15 19:20:14 +0200

    Add a buggy initial version of the algorithm but it works!!

--- NeedlemanWunsch.py
+++ NeedlemanWunsch.py
@@ -16,11 +16,38 @@ def initialise_matrix(seq_a, seq_b):
     seq_a = BLANK + seq_a
     seq_b = BLANK + seq_b
 
-    matrix = np.ndarray(shape=(len(seq_a), len(seq_b)))
+    matrix = np.zeros(shape=(len(seq_a), len(seq_b)), dtype=int)
+    direction_matrix = np.empty(shape=(len(seq_a), len(seq_b)), dtype=str)
 
     for i in range(len(seq_a)):
         for j in range(len(seq_b)):
             if i == 0 and j == 0:
                 matrix[0][0] = 0
+                direction_matrix[0][0] = None
             else:
-                pass
+                diagonal = matrix[i - 1][j - 1] + score_function(seq_a[i], seq_b[j])
+                left = matrix[i][j - 1] + score_function(BLANK, seq_b[j])
+                right = matrix[i - 1][j] + score_function(seq_a[i], BLANK)
+                selected_max = max(left, right, diagonal)
+                matrix[i][j] = selected_max
+                temp_direction = None
+                if selected_max == left:
+                    temp_direction = 'L'
+                elif selected_max == right:
+                    temp_direction = 'R'
+                elif selected_max == diagonal:
+                    temp_direction = 'D'
+
+                direction_matrix[i][j] = temp_direction
+
+    return matrix, direction_matrix
+
+
+def main():
+    A = 'ACCAGGTA'
+    B = 'GAGTTCA'
+
+    matrix, direction_matrix = initialise_matrix(A, B)
+
+
+main()
